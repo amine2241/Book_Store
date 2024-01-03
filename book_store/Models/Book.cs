6eb7d3c6febf30261bad0e57d3296b14e4a61fe1
Mohamed Amine Lakbachi <aminelakbachi21@gmail.com>
2024-01-03 14:56:20 +0100

    added book view and controller

---
+++ book_store/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿namespace book_store.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }   
+        public string Description { get; set; }
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public int YearPublished { get; set; }
+
+        public string Genre { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
