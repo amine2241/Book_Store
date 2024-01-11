53c3e989113796ed80ae6023a9c6c7762ad4f76a
Mohamed Amine Lakbachi <aminelakbachi21@gmail.com>
2024-01-11 18:12:38 +0100

    added category model

--- book_store/Models/Book.cs
+++ book_store/Models/Book.cs
@@ -1,9 +1,14 @@
-﻿namespace book_store.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace book_store.Models
 {
     public class Book
     {
         public int Id { get; set; }
-        public string Name { get; set; }   
+        [Required(ErrorMessage = "Please enter a value")]
+        public string Name { get; set; }
+        [Required, MinLength(4, ErrorMessage = "Minimum length is 2")]
         public string Description { get; set; }
         public string Title { get; set; }
 
@@ -12,8 +17,11 @@
         public int YearPublished { get; set; }
 
         public string Genre { get; set; }
-
+        public Category Category { get; set; }
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a value")]
+        [Column(TypeName = "decimal(8, 2)")]
         public decimal Price { get; set; }
-        public string? ImageUrl { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
