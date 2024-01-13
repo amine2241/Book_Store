99caa553e1d76bff35bbbf0858abb1f9ebe896f0
Mohamed Amine Lakbachi <aminelakbachi21@gmail.com>
2024-01-13 18:38:51 +0100

    CategoriesViewComponent

---
+++ book_store/Data/Components/CategoriesViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace book_store.Data.Components
+{
+    public class CategoriesViewComponent:ViewComponent
+    {
+        private readonly BookdbContext _context; 
+        public CategoriesViewComponent(BookdbContext context)
+        {
+            _context = context; 
+        }
+        public async Task<IViewComponentResult> InvokeAsync() => View(await _context.Categories.ToListAsync());
+    }
+}
