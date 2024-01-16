f34ffde3afc15ff25be96c50f3e34498386b22ab
Mohamed Amine Lakbachi <aminelakbachi21@gmail.com>
2024-01-16 17:09:08 +0100

    added needed category code in book controller

--- book_store/Controllers/BookController.cs
+++ book_store/Controllers/BookController.cs
@@ -2,22 +2,36 @@
 using book_store.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace book_store.Controllers
 {
     [Authorize]
     public class BookController : Controller
     {
-        private readonly BookdbContext _db; 
-        public BookController(BookdbContext db)
+        private readonly BookdbContext _context; 
+        public BookController(BookdbContext context)
         {
-            _db = db;
+            _context =context;
         }
-        public IActionResult Index()
+        public async Task <IActionResult> Index(string categorySlug = "", int p= 1)
         {
-            List<Book> objCategoryList = _db.Books.ToList();
+            int pageSize = 3; 
+            ViewBag.PageSize =p;
+            ViewBag.PageRange = pageSize;
+            ViewBag.CategorySlug =categorySlug;
+            if(categorySlug == "")
+            {
+                ViewBag.TotalPages = (int)Math.Ceiling((decimal)_context.Books.Count() / pageSize);
+                return View(await _context.Books.OrderByDescending(p => p.Id).Skip((p - 1) * pageSize).Take(pageSize).ToListAsync());
+            }
+            Category category = await _context.Categories.Where(c => c.Name == categorySlug).FirstOrDefaultAsync();
+            if (category == null) return RedirectToAction("Index");
 
-            return View(objCategoryList);
+            var productsByCategory = _context.Books.Where(p => p.Id == category.Id);
+            ViewBag.TotalPages = (int)Math.Ceiling((decimal)productsByCategory.Count() / pageSize);
+
+            return View(await productsByCategory.OrderByDescending(p => p.Id).Skip((p - 1) * pageSize).Take(pageSize).ToListAsync());
         }
     }
 }
